386f945251ad25040ff0988fb0be8ba0e194082d
nicholas rotich <nicholas.rotich@gmail.com>
2024-11-13 14:42:21 +0200

    added bybit

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
-from scrape import fetch_delisted, exchanges  # Import your scraping function
+from scrape import fetch_gateio_delisted, fetch_bybit_delisted, exchanges  # Import your scraping function
 
 
 app = FastAPI()
 
 @app.get("/api/delisted/{exchange}")
 async def scrape(exchange: str):
-    data = fetch_delisted(f'{exchanges[0].get(f"url")}')  # Call your scraping function
-    return data               # FastAPI automatically serializes to JSON
+    func_name = f'fetch_{exchange}_delisted'
+    return globals()[func_name](f"{exchanges[0].get('url')}")
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
